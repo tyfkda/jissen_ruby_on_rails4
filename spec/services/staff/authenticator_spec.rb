79b725f4caf33ece222d3adbadda7d57fa915cc4
tyfkda <tyfkda@gmail.com>
2016-01-31 11:16:14 +0900

    認証コードのテスト追加
    * `uninitialized constant Staff::Authenticator (NameError)` が出る場合には、
      `bin/spring stop` を実行してみる：
      [RSpec/Rails: uninitialized constant が出たら bin/spring stop を実行してみると吉 - tkrdの日記 (rails や ubuntu のことなど)](http://d.hatena.ne.jp/tkrd/20140728/1406511209)

---
+++ spec/services/staff/authenticator_spec.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+require 'rails_helper'
+
+describe Staff::Authenticator do
+  describe '#authenticate' do
+    example '正しいパスワードならtrueを返す' do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_truthy
+    end
+
+    example '誤ったパスワードならfalseを返す' do
+      m = build(:staff_member)
+      expect(Staff::Authenticator.new(m).authenticate('xy')).to be_falsy
+    end
+
+    example 'パスワード未設定ならfalseを返す' do
+      m = build(:staff_member, password: nil)
+      expect(Staff::Authenticator.new(m).authenticate(nil)).to be_falsy
+    end
+
+    example '停止フラグが立っていればfalseを返す' do
+      m = build(:staff_member, suspended: true)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_falsy
+    end
+
+    example '開始前ならfalseを返す' do
+      m = build(:staff_member, start_date: Date.tomorrow)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_falsy
+    end
+
+    example '終了後ならfalseを返す' do
+      m = build(:staff_member, end_date: Date.today)
+      expect(Staff::Authenticator.new(m).authenticate('pw')).to be_falsy
+    end
+  end
+end
