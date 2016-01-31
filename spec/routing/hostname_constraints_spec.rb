98e54a8fe98ce17d47c2d63f5d823d747229c5e8
tyfkda <tyfkda@gmail.com>
2016-01-31 14:50:34 +0900

    ルーティングのテスト追加

---
+++ spec/routing/hostname_constraints_spec.rb
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+require 'rails_helper'
+
+describe 'ルーティング' do
+  example '職員トップページ' do
+    expect(get: 'http://baukis.example.com').to route_to(
+      host: 'baukis.example.com',
+      controller: 'staff/top',
+      action: 'index',
+    )
+  end
+
+  example '管理者ログインフォーム' do
+    expect(get: 'http://baukis.example.com/admin/login').to route_to(
+      host: 'baukis.example.com',
+      controller: 'admin/sessions',
+      action: 'new',
+    )
+  end
+
+  example 'ホスト名が対象外ならerrors/not_found' do
+    expect(get: 'http://foo.example.jp').to route_to(
+      controller: 'errors',
+      action: 'routing_error'
+    )
+  end
+
+  example 'ホスト名が対象外ならerrors/not_found' do
+    expect(get: 'http://baukis.example.com/xyz').to route_to(
+      controller: 'errors',
+      action: 'routing_error',
+      anything: 'xyz'
+    )
+  end
+end
