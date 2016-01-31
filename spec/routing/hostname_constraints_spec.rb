73437d3b8df3e4c67231e4a387ac65836a7e4f40
tyfkda <tyfkda@gmail.com>
2016-01-31 14:55:37 +0900

    顧客トップページをホスト名で制限

--- spec/routing/hostname_constraints_spec.rb
+++ spec/routing/hostname_constraints_spec.rb
@@ -18,6 +18,14 @@ describe 'ルーティング' do
     )
   end
 
+  example '顧客トップページ' do
+    expect(get: 'http://example.com/mypage').to route_to(
+      host: 'example.com',
+      controller: 'customer/top',
+      action: 'index',
+    )
+  end
+
   example 'ホスト名が対象外ならerrors/not_found' do
     expect(get: 'http://foo.example.jp').to route_to(
       controller: 'errors',
