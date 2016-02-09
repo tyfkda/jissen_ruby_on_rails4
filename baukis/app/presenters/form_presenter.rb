f5db394883971b05ebb82b93c7492495a5ed6c7c
tyfkda <tyfkda@gmail.com>
2016-02-09 21:58:03 +0900

    プレゼンターで表示用のERBを簡略化

--- baukis/app/presenters/form_presenter.rb
+++ baukis/app/presenters/form_presenter.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class FormPresenter
   include HtmlBuilder
 
@@ -8,5 +9,36 @@ class FormPresenter
   def initialize(form_builder, view_context)
     @form_builder = form_builder
     @view_context = view_context
+  end
+
+  def notes
+    markup(:div, class: 'notes') do |m|
+      m.span '*', class: 'mark'
+      m.text '印のついた項目は入力必須です'
+    end
+  end
+
+  def text_field_block(name, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << label(name, label_text,
+        class: options[:required] ? 'required' : nil)
+      m << text_field(name, options)
+    end
+  end
+
+  def password_field_block(name, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << label(name, label_text,
+        class: options[:required] ? 'required' : nil)
+      m << password_field(name, options)
+    end
+  end
+
+  def date_field_block(name, label_text, options = {})
+    markup(:div, class: 'input-block') do |m|
+      m << label(name, label_text,
+        class: options[:required] ? 'required' : nil)
+      m << text_field(name, options)
+    end
   end
 end
