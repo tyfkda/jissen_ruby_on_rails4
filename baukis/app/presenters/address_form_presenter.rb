af5db80814ee17e106d93e727491c0db16ba5650
tyfkda <tyfkda@gmail.com>
2016-02-27 11:37:59 +0900

    顧客情報の新規登録、編集画面作成

---
+++ baukis/app/presenters/address_form_presenter.rb
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+class AddressFormPresenter < FormPresenter
+  def postal_code_block(name, label_text, options)
+    markup(:div, class: 'input-block') do |m|
+      m << decorated_label(name, label_text, options)
+      m << text_field(name, options)
+      m.span '（７桁の半角数字で入力してください。）', class: 'notes'
+      m << error_messages_for(name)
+    end
+  end
+end
