e3540b4d9832fba7f6271cd9786ac5a2a515ae08
tyfkda <tyfkda@gmail.com>
2016-02-27 12:08:02 +0900

    顧客情報を新規追加・編集できるようにする

--- baukis/app/controllers/staff/customers_controller.rb
+++ baukis/app/controllers/staff/customers_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Staff::CustomersController < Staff::Base
   def index
     @customers = Customer.order(:family_name_kana, :given_name_kana)
@@ -14,5 +15,29 @@ class Staff::CustomersController < Staff::Base
 
   def edit
     @customer_form = Staff::CustomerForm.new(Customer.find(params[:id]))
+  end
+
+  def create
+    @customer_form = Staff::CustomerForm.new
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.save
+      flash.notice = '顧客を追加しました。'
+      redirect_to action: 'index'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'new'
+    end
+  end
+
+  def update
+    @customer_form = Staff::CustomerForm.new(Customer.find(params[:id]))
+    @customer_form.assign_attributes(params[:form])
+    if @customer_form.save
+      flash.notice = '顧客情報を更新しました。'
+      redirect_to action: 'index'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'edit'
+    end
   end
 end
